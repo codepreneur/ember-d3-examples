30ec30a607117c46a9da1381ee3c6bacf9ca365c
codepreneur <contramundee@gmail.com>
2014-11-03 20:35:55 +0000

    added companies and styling

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+	this.resource('companies', {path: '/'}, function(){
+		this.resource('company', {path: '/:company_id'});
+	});
 });
 
 export default Router;
